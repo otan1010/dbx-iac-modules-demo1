7e2da92c818b102aa61a3d12e6af8759af5c984e
Andreas Ottosson <andreas.ottosson@tutamail.com>
2026-02-11 18:54:37 +0100

    updated variables

--- modules/resource_group/variables.tf
+++ modules/resource_group/variables.tf
@@ -2,3 +2,8 @@ variable "name" {
   description = "The name of the resource group."
   type        = string
 }
+
+variable "region" {
+  description = "Azure region/location"
+  type        = string
+}
