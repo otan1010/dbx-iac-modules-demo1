9a6322c27de6be32e8a53a633b984d17bd735d45
otan1010 <andreas.ottosson@tutamail.com>
2026-02-14 18:55:45 +0100

    Update resourcegroup.tf

--- modules/resource_group/resourcegroup.tf
+++ modules/resource_group/resourcegroup.tf
@@ -2,8 +2,3 @@ resource "azurerm_resource_group" "this" {
   name     = var.name
   location = var.location
 }
-
-output "relative_paths" {
-  description = "the relative path terragrunt hopefully outputs"
-  value = "${get_parent_terragrunt_dir()}/${path_relative_to_include()}/terraform.tfstate"
-}
