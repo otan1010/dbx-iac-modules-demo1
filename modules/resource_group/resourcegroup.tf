2d40b180cd5525f44c200289095ba5d02271d38a
otan1010 <andreas.ottosson@tutamail.com>
2026-02-14 18:49:13 +0100

    Update resourcegroup.tf

--- modules/resource_group/resourcegroup.tf
+++ modules/resource_group/resourcegroup.tf
@@ -2,3 +2,7 @@ resource "azurerm_resource_group" "this" {
   name     = var.name
   location = var.location
 }
+
+output {
+  pathh = "${get_parent_terragrunt_dir()}/${path_relative_to_include()}/terraform.tfstate"
+}
