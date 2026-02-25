72c298036d2358803f5904c13d8fca3a9d4d6210
Andreas Ottosson <andreas.ottosson@tutamail.com>
2026-02-25 20:07:26 +0100

    added metastore

--- modules/databricks_metastore/metastore.tf
+++ modules/databricks_metastore/metastore.tf
@@ -1,4 +1,6 @@
-resource "azurerm_resource_group" "this" {
-  name     = var.name
-  location = var.region
+resource "databricks_metastore" "this" {
+  name = var.name
+  region = var.region
+  delta_sharing_scope = "INTERNAL"
+  delta_sharing_recipient_token_lifetime_in_seconds = 1
 }
