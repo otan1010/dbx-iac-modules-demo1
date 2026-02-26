6215fd1ac29785af8765e185262bbd221b10d038
Andreas Ottosson <andreas.ottosson@tutamail.com>
2026-02-26 20:16:53 +0100

    added folders, renamed into main

---
+++ modules/databricks_workspace/main.tf
@@ -0,0 +1,85 @@
+resource "azurerm_resource_group" "this" {
+  name     = "databricks_demo_rg"
+  location = var.region
+}
+
+resource "azurerm_virtual_network" "this" {
+  name                = "demo-databricks-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+}
+
+resource "azurerm_subnet" "public" {
+  name                 = "demo-public-subnet"
+  resource_group_name  = azurerm_resource_group.this.name
+  virtual_network_name = azurerm_virtual_network.this.name
+  address_prefixes     = ["10.0.1.0/24"]
+
+  delegation {
+    name = "demo-databricks-del"
+
+    service_delegation {
+      actions = [
+        "Microsoft.Network/virtualNetworks/subnets/join/action",
+        "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action",
+        "Microsoft.Network/virtualNetworks/subnets/unprepareNetworkPolicies/action",
+      ]
+      name = "Microsoft.Databricks/workspaces"
+    }
+  }
+}
+
+resource "azurerm_subnet" "private" {
+  name                 = "demo-private-subnet"
+  resource_group_name  = azurerm_resource_group.this.name
+  virtual_network_name = azurerm_virtual_network.this.name
+  address_prefixes     = ["10.0.2.0/24"]
+
+  delegation {
+    name = "demo-databricks-del"
+
+    service_delegation {
+      actions = [
+        "Microsoft.Network/virtualNetworks/subnets/join/action",
+        "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action",
+        "Microsoft.Network/virtualNetworks/subnets/unprepareNetworkPolicies/action",
+      ]
+      name = "Microsoft.Databricks/workspaces"
+    }
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "private" {
+  subnet_id                 = azurerm_subnet.private.id
+  network_security_group_id = azurerm_network_security_group.this.id
+}
+
+resource "azurerm_subnet_network_security_group_association" "public" {
+  subnet_id                 = azurerm_subnet.public.id
+  network_security_group_id = azurerm_network_security_group.this.id
+}
+
+resource "azurerm_network_security_group" "this" {
+  name                = "demo-databricks-nsg"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+}
+
+resource "azurerm_databricks_workspace" "this" {
+  name                        = "databricks_demo_workspace"
+  resource_group_name         = azurerm_resource_group.this.name
+  location                    = azurerm_resource_group.this.location
+  sku                         = "premium"
+  managed_resource_group_name = "databricks_demo_rg_managed"
+
+	  custom_parameters {
+      no_public_ip        = true
+      public_subnet_name  = azurerm_subnet.public.name
+      private_subnet_name = azurerm_subnet.private.name
+      virtual_network_id  = azurerm_virtual_network.this.id
+
+      public_subnet_network_security_group_association_id  = azurerm_subnet_network_security_group_association.public.id
+      private_subnet_network_security_group_association_id = azurerm_subnet_network_security_group_association.private.id
+  }
+}
