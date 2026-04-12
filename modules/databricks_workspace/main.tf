cad7dcd68d64721b5cd015e83b5ebdbfd49c60b4
otan1010 <andreas.ottosson@tutamail.com>
2026-04-12 19:54:14 +0200

    Update main.tf

--- modules/databricks_workspace/main.tf
+++ modules/databricks_workspace/main.tf
@@ -29,8 +29,8 @@ resource "azurerm_subnet" "public" {
 resource "azurerm_subnet" "private" {
   name                 = "demo-private-subnet"
   resource_group_name  = var.rg
-  virtual_network_name = "vnet-private"
-  #virtual_network_name = azurerm_virtual_network.this.name
+  #virtual_network_name = "vnet-private"
+  virtual_network_name = azurerm_virtual_network.this.name
   address_prefixes     = ["10.0.2.0/24"]
 
   delegation {
