3f5df49e237863459b2fcacf0a7152f71c24477a
otan1010 <andreas.ottosson@tutamail.com>
2026-04-17 20:31:06 +0200

    Update main.tf

--- modules/databricks_account/main.tf
+++ modules/databricks_account/main.tf
@@ -3,3 +3,8 @@ resource "databricks_disable_legacy_features_setting" "this" {
     value = true
   }
 }
+
+resource "databricks_group_role" "account_admin" {
+  group_id = var.dbx_acc_adm_grp_id
+  role     = "account_admin"
+}
