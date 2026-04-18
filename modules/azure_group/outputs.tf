7ac8b850e14423795c3e7b2bcd30db74274a8dc4
otan1010 <andreas.ottosson@tutamail.com>
2026-04-18 20:52:08 +0200

    Update outputs.tf

--- modules/azure_group/outputs.tf
+++ modules/azure_group/outputs.tf
@@ -2,3 +2,8 @@ output "entraid_group_id" {
   description = "The Object ID of the created EntraID group."
   value       = azuread_group.this.id
 }
+
+output "entraid_group_display_name" {
+  description = "The display name of the created Entra ID group."
+  value       = azuread_group.this.display_name
+}
