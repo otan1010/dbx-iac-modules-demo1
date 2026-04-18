c9c3a8f2fa2ec62eb1e3aa6f0140130a93ac9171
otan1010 <andreas.ottosson@tutamail.com>
2026-04-18 20:52:40 +0200

    Update variables.tf

--- modules/databricks_account/variables.tf
+++ modules/databricks_account/variables.tf
@@ -2,3 +2,8 @@ variable "dbx_acc_adm_grp_id" {
   type        = string
   description = "The ID of the Azure AD group to make account admin."
 }
+
+variable "dbx_acc_adm_grp_display_name" {
+  type        = string
+  description = "The display name of the Azure AD group to make account admin."
+}
