cb9200b83e742daf0dd8aea6ae2244b092cafa13
otan1010 <andreas.ottosson@tutamail.com>
2026-04-19 09:25:25 +0200

    Delete modules/databricks_workspace/identity.tf

--- modules/databricks_workspace/identity.tf
+++
@@ -1,6 +0,0 @@
-resource "databricks_mws_permission_assignment" "workspace_admin_setup" {
-  provider     = databricks.account
-  workspace_id = var.workspace_id
-  principal_id = var.dbx_acc_adm_grp_object_id
-  permissions  = ["ADMIN"]
-}
