027bbb2d12b87fcfff139a73e9bd45ce07d09503
otan1010 <andreas.ottosson@tutamail.com>
2026-04-19 09:25:39 +0200

    Update variables.tf

--- modules/databricks_workspace/variables.tf
+++ modules/databricks_workspace/variables.tf
@@ -17,8 +17,3 @@ variable "databricks_metastore_id" {
   description = "ID for Databricks Metastore."
   type        = string
 }
-
-variable "dbx_acc_adm_grp_object_id" {
-  description = "ID for Databricks EntraID admin group."
-  type        = string
-}
