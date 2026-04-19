f8e8e7eb3772e7885db601d892d0b6e7d79c1508
otan1010 <andreas.ottosson@tutamail.com>
2026-04-19 11:27:19 +0200

    Update variables.tf

--- modules/databricks_workspace/variables.tf
+++ modules/databricks_workspace/variables.tf
@@ -17,3 +17,8 @@ variable "databricks_metastore_id" {
   description = "ID for Databricks Metastore."
   type        = string
 }
+
+variable "dbx_ws_adm_grp_object_id" {
+  type        = string
+  description = "The object ID of the Azure AD group to make workspace admin."
+}
