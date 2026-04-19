e7888e39ad6b74cf7fadbfcfdbdf060c78c99b26
otan1010 <andreas.ottosson@tutamail.com>
2026-04-19 09:10:18 +0200

    Update variables.tf

--- modules/databricks_account/variables.tf
+++ modules/databricks_account/variables.tf
@@ -7,3 +7,13 @@ variable "dbx_acc_adm_grp_display_name" {
   type        = string
   description = "The display name of the Azure AD group to make account admin."
 }
+
+variable "dbx_mstore_adm_grp_object_id" {
+  type        = string
+  description = "The object ID of the Azure AD group for global metastore admin."
+}
+
+variable "dbx_mstore_adm_grp_display_name" {
+  type        = string
+  description = "The display name of the Azure AD group for global metastore admin."
+}
