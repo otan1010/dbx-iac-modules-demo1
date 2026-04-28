ac606a5ef186f8a15bbc9b35b1e2e00c856ba4e8
otan1010 <andreas.ottosson@tutamail.com>
2026-04-28 19:16:48 +0200

    Update main.tf

--- modules/databricks_workspace/main.tf
+++ modules/databricks_workspace/main.tf
@@ -87,7 +87,7 @@ resource "databricks_metastore_assignment" "this" {
 }
 
 resource "databricks_group" "dbx_ws_adm_grp" {
-  provider     = databricks.account_level
+  #provider     = databricks.account_level
   display_name = var.dbx_ws_adm_grp_display_name
   external_id  = var.dbx_ws_adm_grp_object_id
 }
